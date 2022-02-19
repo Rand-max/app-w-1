85addc17acab7615b19e439ef7aad5b8c6240a8d
Rand-max <57353839+Rand-max@users.noreply.github.com>
2022-02-19 22:23:47 +0800

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>110819042 謝博帆</Text>
       <StatusBar style="auto" />
     </View>
   );
